a2ef5121e2cee3bf0cb9cc5a17e35ea74bb345a9
Elysia Gabe <elysiagabe@gmail.com>
2020-10-20 23:02:53 -0700

    Adds emergency icon to footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Box } from '@material-ui/core';
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
 
 const useStyles = makeStyles((theme) => ({
     footer: {
@@ -8,6 +9,12 @@ const useStyles = makeStyles((theme) => ({
         bottom: 0,
         width: '100%',
         height: '2rem',
+    },
+    icon: {
+        position: 'absolute',
+        // border: '1px solid red',
+        bottom: 16,
+        right: 28,
     }
 }))
 
@@ -20,6 +27,9 @@ const Footer = () => {
                 <Typography variant="body2" color="textSecondary" align="center">
                     &copy; {new Date().getFullYear()} ReFrame by Elysia Gabe
                 </Typography>
+            </Box>
+            <Box className={classes.icon}>
+                <ReportProblemIcon color="error" fontSize="large" />
             </Box>
         </footer>
     );
